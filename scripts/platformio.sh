d746dd9ec26a39aca996d76575feae74c4571180
PavelProjects <pavelprojects52@gmail.com>
2024-11-28 23:43:44 +0300

    auto deploy

--- scripts/platformio.sh
+++ scripts/platformio.sh
@@ -1,8 +1,21 @@
-LIB_PROJECT_DIR="$HOME/projects/smart_thing/smart_thing_lib"
-HEADER_DIR="$LIB_PROJECT_DIR/lib/smartthing/src/net/rest"
+if [ -z ${npm_config_dest} ];
+then
+  LIB_DIR="$HOME/projects/smart_thing/smart_thing_lib/lib/smartthing"
+else
+  LIB_DIR="$npm_config_dest"
+fi
 
-if [ ! -d "$HEADER_DIR" ]; then
-  echo 
-fi && \
-scp -r ./dist/WebPageAssets.h $HEADER_DIR/WebPageAssets.h && \
-echo "Copied header file WebPageAssets.h in $HEADER_DIR"
+FULL_PATH="$LIB_DIR/src/net/rest"
+
+if [ ! -d "$FULL_PATH" ]; then
+  echo "Bad destanation path: $LIB_DIR (resolved to $FULL_PATH)"
+  exit 1
+fi
+
+scp -r ./dist/WebPageAssets.h $FULL_PATH/WebPageAssets.h
+if [ $? -eq 0 ]; then
+  echo "Copied header file WebPageAssets.h in $FULL_PATH"
+else
+  echo "Failed to deploy header file"
+  exit 1
+fi
